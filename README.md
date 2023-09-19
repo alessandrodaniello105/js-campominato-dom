e5e109d886420d7921ab781d2bf2a94af5d1a93f
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-19 15:22:35 +0200

    [edit]: README/better pseudo-code

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
-Griglia Campo Minato
+Campo Minato
 ===
-
-1. Creo un container dove inserisco i miei quadrati e lo stilo (flex, ecc)
-1. Prima in maniera statica, aggiungo i miei quadrati nell'html
-1. Creo lo stile/la classe del quadrato in css, inizialmente per una griglia 10 * 10
-1. Stilo la classe 'clicked'
-1. Definisco la variabile del container e della top-bar (dove andrà la selezione dei livelli)
-1. Definisco la funzione di generazione del quadrato, dando un custom ID basato sull'index del ciclo 
-1. Creo la funzione di reset del container e la invoco
-1. Creo il ciclo di generazione dei 100 quadrati
-1. Creo la funzione di generazione griglia e ci inserisco dentro la nostra generazione quadrati
-1. Creo la funzione di generazione del bottone di start, che al click si auto-cancella e invoca la funzione di generazione griglia
-1. Controllo quale difficoltà è stata selezionata
-
 ## Bombe, vittoria e fine gioco
-12. Creo un counter per il punteggio
+1. Creo un counter per il punteggio
 1. Al click del quadrato rimuovo la funzione click dal quadrato cliccato. Poi '++' counte
 1. Creo una lista globale vuota che conterrà le mie bombe
 1. Creo una funzione di generazione random di 16 numeri con range min(1)-max(numero quadrati) e con verifica di univocità
--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-// 5. Definisco la variabile del container
+// ELEMENTS
 const container   = document.querySelector('.container');
 const topBar      = document.querySelector('.top-bar');
 const diffSelector = document.getElementById('difficulty-selector');
+
 let started = false;
 
 // START BUTTON
@@ -9,16 +10,14 @@ const buttonStart = document.createElement('button');
 buttonStart.innerHTML = 'START';
 topBar.append(buttonStart);
 
-// const buttonStart = document.getElementById('button');
-
 let squaresNumber; 
 
 
 
-// 12. Creo un counter per il punteggio
+// 1. Creo un counter per il punteggio
 let counter = 0;
 
-// 14. Creo una lista globale vuota che conterrà le mie bombe
+// 3. Creo una lista globale vuota che conterrà le mie bombe
 const bombs = [];
 
 
@@ -32,7 +31,6 @@ getStarted();
 
 function init(num){
 
-  // 8. Creo il ciclo di generazione dei 100 quadrati
   for (let i = 1; i <= num; i++){
 
     squareGeneration(i);
@@ -42,8 +40,8 @@ function init(num){
 };
 
 
-// 6. Definisco la funzione di generazione del quadrato, dando un custom ID basato sull'index del ciclo 
 console.log('INIZIO---', counter);
+
 function squareGeneration(index){
 
   let newSquare = document.createElement('div');
@@ -55,7 +53,7 @@ function squareGeneration(index){
   newSquare._squareID = index;
 
 
-  // 13. Al click del quadrato rimuovo la funzione click dal quadrato cliccato. Poi '++' counter.
+  // 2. Al click del quadrato rimuovo la funzione click dal quadrato cliccato. Poi '++' counter.
   newSquare.addEventListener('click', handleSquareClick);
   
   function handleSquareClick(){
@@ -73,18 +71,12 @@ function squareGeneration(index){
 };
 
 
-// 7. Creo la funzione di reset del container e la invoco
 function reset(){
   container.innerHTML = '';
   bombs.splice(0, bombs.length);
 };
 
 
-
-// 9. Creo la funzione di generazione griglia e ci inserisco dentro la nostra generazione quadrati
-
-
-// 10. Creo la funzione di generazione del bottone di start, che al click si auto-cancella e invoca la funzione di generazione griglia
 function getStarted(){
 
   buttonStart.addEventListener('click', function(){
@@ -99,8 +91,6 @@ function getStarted(){
   
 };
 
-
-// 11. Controllo quale difficoltà è stata selezionata
 function choiceDifficulty(value) {
 
   if (value == 1) return 100;
@@ -112,9 +102,7 @@ function choiceDifficulty(value) {
 };
 
 
-// 15. Creo una funzione di generazione random di 16 numeri con range min(1)-max(numero quadrati) e con verifica di univocità
-
-console.log(bombs);
+// 4. Creo una funzione di generazione random di 16 numeri con range min(1)-max(numero quadrati) e con verifica di univocità
 function generateBombs(maxSquares){
   
   let targetBomb = 16
